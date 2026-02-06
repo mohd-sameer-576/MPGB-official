2d6dfe345447e1f6ac24e4aed0eb550476ea8436
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2026-02-06 15:31:31 +0530

    ui fix

--- frontend/mpgb/src/pages/Shop.jsx
+++ frontend/mpgb/src/pages/Shop.jsx
@@ -55,6 +55,7 @@ const Shop = () => {
 
   if (loading) return (
     <div className="flex justify-center items-center h-screen bg-white">
+    <h2>Fetching Products please wait</h2>
       <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-orange-600"></div>
     </div>
   );
