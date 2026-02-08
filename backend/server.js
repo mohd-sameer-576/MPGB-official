e676846c7ff94d290d1e2ddc6b5aca33dcf22a1b
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2026-02-08 16:46:53 +0530

    server changes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ connectDB();
 
 // Configure CORS to allow credentials (cookies)
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || true,
+  origin:[ 
+    process.env.FRONTEND_URL, "http://localhost:5173"],
   credentials: true,
 };
 
